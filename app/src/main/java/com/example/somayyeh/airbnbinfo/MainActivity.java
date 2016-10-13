87c8e7ac6310a31aac19e7746288ccc1ecfa9498
Somayyeh Zahedian <s.zahedian@gmail.com>
2016-10-13 12:42:28 +1100

    Commit the primary version of project

---
+++ app/src/main/java/com/example/somayyeh/airbnbinfo/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.somayyeh.airbnbinfo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String htmlText = " %s ";
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.loadData(getString(R.string.hello), "text/html; charset=utf-8", "utf-8");
+    }
+}
